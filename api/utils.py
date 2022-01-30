9266bb4dbc9106899d3250f54e35e7888747a5f4
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2022-01-30 10:11:27 +0000

    Made few changes

--- api/utils.py
+++ api/utils.py
@@ -7,6 +7,8 @@ def write_to_tmp(file):
     file_path = f'temp/{file}'
     with open(file_path, 'wb+') as f:
         f.write(file.read())
+
+    print(f'File written :{os.path.isfile(file_path)}')
     print(f'File added :{file_path}')
     return file_path
 
