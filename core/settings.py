734756260607b97763b02732290327e51b26c81e
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2022-01-30 08:32:00 +0000

    Updated settiongs and views for user.

--- core/settings.py
+++ core/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+ 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -127,8 +127,8 @@ KAFKA_BROKER_URL = ''
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
