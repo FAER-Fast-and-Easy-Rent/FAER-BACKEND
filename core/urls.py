d1876d79cadc38bc025aed2aa4ca31c352bcdd01
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2022-02-10 09:04:50 +0000

    Added static files url

--- core/urls.py
+++ core/urls.py
@@ -13,14 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include
 from . import views
+from django.urls import path
+from django.contrib import admin
+from django.urls.conf import include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
-
 ]
+
+urlpatterns += staticfiles_urlpatterns()
