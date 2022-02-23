3358c0802d6486556e6cb659f9605cd0ce403aec
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2022-02-23 11:07:51 +0000

    Updated User info retrieval

--- api/models.py
+++ api/models.py
@@ -56,7 +56,7 @@ class Room(models.Model):
     # )
     room_id = models.AutoField(primary_key=True)
     price = models.IntegerField()
-    title = models.CharField(max_length=255, unique=True)
+    title = models.CharField(max_length=255)
     description = models.TextField(max_length=500, blank=True, default='')
 
     home_type = models.CharField(max_length=100)  # ,choices=home_types)
