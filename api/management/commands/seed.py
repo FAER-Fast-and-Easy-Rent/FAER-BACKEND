440513b70a175c3498f36b9261f0592b1e10c95f
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2022-03-01 17:05:33 +0000

    Updated settings.

--- api/management/commands/seed.py
+++ api/management/commands/seed.py
@@ -5,8 +5,12 @@ from api.data_loader import seed_users, seed_vehicles, seed_rooms
 class Command(BaseCommand):
     help = 'Seed database'
 
+    def add_arguments(self, parser):
+        parser.add_argument('seed', nargs='?', type=bool, help='seed rooms and vehicles')
+
     def handle(self, *args, **options):
 
         seed_users()
-        seed_rooms()
-        seed_vehicles()
+        if options['seed']:
+            seed_rooms()
+            seed_vehicles()
