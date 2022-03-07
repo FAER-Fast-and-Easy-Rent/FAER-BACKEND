282ad4288f440398708c2bbd361951833c08d61a
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2022-03-07 12:56:41 +0000

    Added to admin

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
-from .models import Media, Room
+from .models import Media, Room, Vehicle, Reservation
 
-admin.site.register(Room)
-admin.site.register(Media)
+
+class RoomAdmin(admin.ModelAdmin):
+    fields = ('title', 'price')
+
+
+class VehicleAdmin(admin.ModelAdmin):
+    fields = ('name', 'price')
+
+
+class MediaAdmin(admin.ModelAdmin):
+    fields = ('file_name', 'url')
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    fields = ('total', 'created_at')
+
+
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Vehicle, VehicleAdmin)
+admin.site.register(Media, MediaAdmin)
+admin.site.register(Reservation, ReservationAdmin)
 # Register your models here.
